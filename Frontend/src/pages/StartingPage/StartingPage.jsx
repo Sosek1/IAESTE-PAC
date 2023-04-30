e1e76ed7b5e05352c3675c1386c6dd80c61d1d2d
Sosek1 <68021413+Sosek1@users.noreply.github.com>
2023-04-30 17:25:38 +0200

    install mui

--- Frontend/src/pages/StartingPage/StartingPage.jsx
+++ Frontend/src/pages/StartingPage/StartingPage.jsx
@@ -2,7 +2,11 @@ import classes from "./StartingPage.module.css";
 import logo from "../../assets/logo-white3.png";
 
 const StartingPage = () => {
-  return <p>StartingPage</p>;
+  return (
+    <>
+      <p>StartingPage</p>
+    </>
+  );
 };
 
 export default StartingPage;
