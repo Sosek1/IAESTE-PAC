b7152573b48f7db1066ca33962c033405cab8821
Szymon-Nowaq <117742564+Szymon-Nowaq@users.noreply.github.com>
2023-05-02 00:46:07 +0200

    StartingPage

--- Frontend/src/pages/StartingPage/StartingPage.jsx
+++ Frontend/src/pages/StartingPage/StartingPage.jsx
@@ -3,10 +3,12 @@ import logo from "../../assets/logo-white3.png";
 
 const StartingPage = () => {
   return (
+    <main>
       <header>
         <img src = {logo}/>
         <h1>IAESTinder</h1>
-      </header>  
+      </header>
+    </main>  
   );
 };
 
