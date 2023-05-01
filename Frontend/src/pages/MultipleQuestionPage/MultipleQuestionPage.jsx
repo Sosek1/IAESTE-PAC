c530b692a63e2faf94407c411195c1bf473abce7
Szymon Skrzypczyk <77851583+SzymonSkrzypczyk@users.noreply.github.com>
2023-05-01 12:16:15 +0200

    pierwsze cos
    wyglada to 2/10 poki co XD

--- Frontend/src/pages/MultipleQuestionPage/MultipleQuestionPage.jsx
+++ Frontend/src/pages/MultipleQuestionPage/MultipleQuestionPage.jsx
@@ -1,8 +1,38 @@
 import classes from "./MultipleQuestionPage.module.css";
 import logo from "../../assets/logo-white3.png";
+import WhatshotIcon from '@mui/icons-material/Whatshot';
 
 const MultipleQuestionPage = () => {
-  return <p>Multiple questions</p>;
+  return <div>
+    <div className={classes.progress}></div>
+    <WhatshotIcon className={classes.icon}></WhatshotIcon>
+    <div className={classes.container}>
+      <header className={classes.header}>
+        <h1 className={classes.title}>
+          Passions
+        </h1>
+        <h4 className={classes.text}>
+          Prosty Tekst
+        </h4>
+      </header>
+      <ul className={classes.list}>
+        <li className={classes.item}>sssssssssssś</li>
+        <li className={classes.item}>XD</li>
+        <li className={classes.item}>XD</li>
+        <li className={classes.item}>XD</li>
+        <li className={classes.item}>XD</li>
+        <li className={classes.item}>XD</li>  
+        <li className={classes.item}>sssssssssssś</li>
+        <li className={classes.item}>XD</li>
+        <li className={classes.item}>XD</li>
+        <li className={classes.item}>XD</li>
+        <li className={classes.item}>XD</li>
+        <li className={classes.item}>XD</li>    
+        
+      </ul>
+      <button className={classes.continue}>Continue</button>
+    </div>
+  </div>;
 };
 
 export default MultipleQuestionPage;
