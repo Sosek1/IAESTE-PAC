8480bd4bba273e2b3b7360fe374783eeaa6b6940
Szymon Skrzypczyk <77851583+SzymonSkrzypczyk@users.noreply.github.com>
2023-05-02 09:22:13 +0200

    jeszcze bez js

--- Frontend/src/pages/MultipleQuestionPage/MultipleQuestionPage.jsx
+++ Frontend/src/pages/MultipleQuestionPage/MultipleQuestionPage.jsx
@@ -3,36 +3,37 @@ import logo from "../../assets/logo-white3.png";
 import WhatshotIcon from '@mui/icons-material/Whatshot';
 
 const MultipleQuestionPage = () => {
-  return <div>
-    <div className={classes.progress}></div>
-    <WhatshotIcon className={classes.icon}></WhatshotIcon>
-    <div className={classes.container}>
-      <header className={classes.header}>
-        <h1 className={classes.title}>
-          Passions
-        </h1>
-        <h4 className={classes.text}>
-          Prosty Tekst
-        </h4>
-      </header>
-      <ul className={classes.list}>
-        <li className={classes.item}>sssssssssssś</li>
-        <li className={classes.item}>XD</li>
-        <li className={classes.item}>XD</li>
-        <li className={classes.item}>XD</li>
-        <li className={classes.item}>XD</li>
-        <li className={classes.item}>XD</li>  
-        <li className={classes.item}>sssssssssssś</li>
-        <li className={classes.item}>XD</li>
-        <li className={classes.item}>XD</li>
-        <li className={classes.item}>XD</li>
-        <li className={classes.item}>XD</li>
-        <li className={classes.item}>XD</li>    
-        
-      </ul>
-      <button className={classes.continue}>Continue</button>
-    </div>
-  </div>;
+  return <div className={classes.container}>
+  <div className={classes.progress_bar_container}>
+      <div className={classes.progress_bar_fill}></div>
+  </div>
+  <div>
+      <WhatshotIcon className={classes.icon}></WhatshotIcon>
+  </div>
+  <header>
+      <h1 className={classes.title}>Passions</h1>
+      <p className={classes.text}>Let everyone know what you're passionate about, by adding it to your profile.</p>
+  </header>
+  <ul className={classes.list}>
+      <li className={classes.list_item}>1</li>
+      <li className={classes.list_item}>2</li>
+      <li className={classes.list_item}>3</li>
+      <li className={classes.list_item}>4</li>
+      <li className={classes.list_item}>5</li>
+      <li className={classes.list_item}>6</li>
+      <li className={classes.list_item}>1</li>
+      <li className={classes.list_item}>2</li>
+      <li className={classes.list_item}>3</li>
+      <li className={classes.list_item}>4</li>
+      <li className={classes.list_item}>5</li>
+      <li className={classes.list_item}>6</li>
+      <li className={classes.list_item}>1</li>
+      <li className={classes.list_item}>2</li>
+      <li className={classes.list_item}>3</li>
+      <li className={classes.list_item}>1</li>
+  </ul>
+  <button className={classes.large_button}>Continue</button>
+</div>;
 };
 
 export default MultipleQuestionPage;
