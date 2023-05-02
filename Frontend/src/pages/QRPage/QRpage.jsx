bbe548e95d327a3ba2797f398534a0063d2b6e93
PiotrKur <pkurnicki@student.agh.edu.pl>
2023-05-02 19:58:54 +0200

    pierwsze podejscie do QR

--- Frontend/src/pages/QRPage/QRpage.jsx
+++ Frontend/src/pages/QRPage/QRpage.jsx
@@ -1,8 +1,24 @@
 import classes from "./QRPage.module.css";
-import logo from "../../assets/logo-white3.png";
+import logo from "../../assets/tinder-logo-red.png";
+import QR from "../../assets/QR1.png";
 
 const QRpage = () => {
-  return <p>Qr </p>;
+  return (
+    <main className={classes.QRPage}>
+      <header>
+        <img src={logo} />
+        <h1>Sprawdź IAESTinder</h1>
+      </header>
+
+      <p>
+        Dzięki zeskanowaniu kodu QR będziesz mógł skorzystać z naszej aplikacji
+        w swoim telefonie!
+      </p>
+      <p className={classes.QRPage2}>
+        <img src={QR} />
+      </p>
+    </main>
+  );
 };
 
 export default QRpage;
