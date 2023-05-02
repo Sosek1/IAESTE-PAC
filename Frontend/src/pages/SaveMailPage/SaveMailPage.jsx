812822d50d4f08a6a8c4eae408778db33156f978
kSuroweczka <92479853+kSuroweczka@users.noreply.github.com>
2023-05-02 22:34:52 +0200

    SaveMailPage
    first

--- Frontend/src/pages/SaveMailPage/SaveMailPage.jsx
+++ Frontend/src/pages/SaveMailPage/SaveMailPage.jsx
@@ -1,8 +1,48 @@
+// @ts-ignore
 import classes from "./SaveMailPage.module.css";
-import logo from "../../assets/logo-white3.png";
+// import logo from "../../assets/logo-white3.png";
+import React from "react";
+import { useState } from "react";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 
 const SaveMailPage = () => {
-  return <p>SaveMail</p>;
+  const [email, setEmail] = useState(false);
+  const changecolor = () => {
+    setEmail(true);
+  };
+  return (
+    <>
+      <main className={classes.SaveMailPage}>
+        <section>
+          <button type="button" className={classes.backButton}>
+            <ChevronLeftIcon style={{ fontSize: "60px", color: "gray" }} />
+          </button>
+          <h1 className={classes.header}>What's your email?</h1>
+
+          <p className={classes.paragraph}>
+            Don't lose access to your account, verify your email.
+          </p>
+
+          <div className={classes.SearchBar}>
+            <form action="">
+              <input
+                onChange={changecolor}
+                type="text"
+                placeholder="Enter email"
+                name="q"
+              />
+            </form>
+          </div>
+        </section>
+        <button
+          type="button"
+          className={email ? classes.sendBottomColor : classes.sendBottomGray}
+        >
+          Send
+        </button>
+      </main>
+    </>
+  );
 };
 
 export default SaveMailPage;
