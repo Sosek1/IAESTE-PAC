abf1666b2f740ba54e9838a37ec4a46eed078f9f
Sosek1 <68021413+Sosek1@users.noreply.github.com>
2023-05-04 23:32:48 +0200

    refactor: change MultipleQuestionsPage

--- Frontend/src/pages/MultipleQuestionPage/MultipleQuestionPage.jsx
+++ Frontend/src/pages/MultipleQuestionPage/MultipleQuestionPage.jsx
@@ -1,90 +1,89 @@
-import classes from "./MultipleQuestionPage.module.css";
-// import logo from "../../assets/logo-white3.png";
 import { useState } from "react";
+import classes from "./MultipleQuestionPage.module.css";
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 
-
 const MultipleQuestionPage = () => {
-    //const [active, setActive] = useState(false);
-    //const changeState = (value) => {
-    //    setActive(value);
-    //};
-    let selected = 0;
-    let selected_tags = [];
+  const [randomWords, setRandomWords] = useState([
+    { text: "Giraffe", marked: false, lastMarked: false },
+    { text: "Penguin", marked: false, lastMarked: false },
+    { text: "Cinnamon", marked: false, lastMarked: false },
+    { text: "Banana", marked: false, lastMarked: false },
+    { text: "Thunder", marked: false, lastMarked: false },
+    { text: "Lighthouse", marked: false, lastMarked: false },
+    { text: "Lavender", marked: false, lastMarked: false },
+    { text: "Honeycomb", marked: false, lastMarked: false },
+    { text: "Frosting", marked: false, lastMarked: false },
+    { text: "Sapphire", marked: false, lastMarked: false },
+    { text: "Butterfly", marked: false, lastMarked: false },
+    { text: "Pluto", marked: false, lastMarked: false },
+    { text: "Galaxy", marked: false, lastMarked: false },
+    { text: "Mushroom", marked: false, lastMarked: false },
+  ]);
 
-    function add(event){
-        if(event.target.className.includes('_selected_')){
-            event.target.className = classes.list_item;
-            selected--;
-        }
-        else if (selected < 5){
-            event.target.className = classes.selected;
-            selected++;
-        }
-        const but = document.getElementById('but');
-        but.textContent = `Continue ${selected}/5`;
-        if(selected > 0){
-            // problemy z changeState(psuje sie odliczanie) ;//
-            // dlatego prymityne ale dzialajace rozwiazanie
-            // gdy uzywam useState wtedy tak wyglada button className={active? classes.large_button : classes.disabled_large_button}
-            console.log("yo");
-            but.className = classes.large_button;
-        }
-        else{
-            console.log("not yo");
-            but.className = classes.disabled_large_button;
-        }
-    }
+  const [counter, setCounter] = useState(0);
 
-    const clicked = () => {
-        if(active){
-            selected_tags = [];
-            for(let el of document.getElementsByTagName('li')){
-                if(el.className.includes('_selected_')){
-                    selected_tags.push(el.textContent);
-        }
-    }
-    console.log(selected_tags[1]);
-    }
+  const wordClickHanlder = (obj) => {
+    const copiedList = [...randomWords];
+    const clickedWord = copiedList.find((item) => item.text == obj.text);
+    clickedWord.marked = !clickedWord.marked;
+
+    if (counter === 5) {
+      if (clickedWord.marked) {
+        clickedWord.marked == false;
+        setCounter((prev) => prev - 1);
+      } else {
+        clickedWord.marked == true;
+      }
     }
 
-    return <main className={classes.container}>
-    <div className={classes.progress_bar_container}>
+    if (clickedWord.marked) {
+      setCounter((prev) => prev + 1);
+    }
+    if (!clickedWord.marked) {
+      setCounter((prev) => prev - 1);
+    }
+
+    setRandomWords(copiedList);
+  };
+
+  return (
+    <main className={classes.container}>
+      <div className={classes.progress_bar_container}>
         <div className={classes.progress_bar_fill}></div>
-    </div>
-    <button>
-        <ChevronLeftIcon className={classes.icon}></ChevronLeftIcon>
-    </button>
-    <header className={classes.head}>
+      </div>
+      <ChevronLeftIcon
+        style={{
+          marginTop: "20px",
+          marginBottom: "15px",
+          fontSize: "50px",
+          color: "#b0b0b0",
+        }}
+      ></ChevronLeftIcon>
+      <header className={classes.head}>
         <h1 className={classes.title}>Passions</h1>
-        <p className={classes.text}>Let everyone know what you're passionate about,<br></br> by adding it to your profile.</p>
-    </header>
-    <ul className={classes.list}>
-        <li className={classes.list_item} onClick={add}>jazda na rowerze</li>
-        <li className={classes.list_item} onClick={add}>cos 123123421</li>
-        <li className={classes.list_item} onClick={add}>dlugie</li>
-        <li className={classes.list_item} onClick={add}>4</li>
-        <li className={classes.list_item} onClick={add}>5</li>
-        <li className={classes.list_item} onClick={add}>6</li>
-        <li className={classes.list_item} onClick={add}>1</li>
-        <li className={classes.list_item} onClick={add}>2</li>
-        <li className={classes.list_item} onClick={add}>4</li>
-        <li className={classes.list_item} onClick={add}>5</li>
-        <li className={classes.list_item} onClick={add}>6</li>
-        <li className={classes.list_item} onClick={add}>1</li>
-        <li className={classes.list_item} onClick={add}>2</li>
-        <li className={classes.list_item} onClick={add}>3</li>
-        <li className={classes.list_item} onClick={add}>1</li>
-        <li className={classes.list_item} onClick={add}>4</li>
-        <li className={classes.list_item} onClick={add}>5</li>
-        <li className={classes.list_item} onClick={add}>6</li>
-        <li className={classes.list_item} onClick={add}>1</li>
-        <li className={classes.list_item} onClick={add}>2</li>
-        <li className={classes.list_item} onClick={add}>3</li>
-        <li className={classes.list_item} onClick={add}>1</li>
-    </ul>
-    <button className={classes.disabled_large_button} id='but' onClick={clicked}>Continue 0/5</button>
-</main>;
+        <p className={classes.text}>
+          Let everyone know what you're passionate about,<br></br> by adding it
+          to your profile.
+        </p>
+      </header>
+      <ul className={classes.list}>
+        {randomWords.map((word, index) => (
+          <li
+            className={`${classes.list_item} ${
+              word.marked ? classes.list_item_active : ""
+            } `}
+            key={index}
+            onClick={() => wordClickHanlder(word)}
+          >
+            {word.text}
+          </li>
+        ))}
+      </ul>
+      <button
+        className={classes.large_button}
+      >{`Kontynuuj ${counter}/5`}</button>
+    </main>
+  );
 };
 
 export default MultipleQuestionPage;
