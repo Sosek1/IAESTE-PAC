4758d86f0ec2370dc5b53d9ea5abde625db385a3
Kuba Leszczyński <121136691+Dayl3n@users.noreply.github.com>
2023-05-08 21:15:11 +0200

    Update SingleQuestionPage.jsx

--- Frontend/src/pages/SingleQuestionPage/SingleQuestionPage.jsx
+++ Frontend/src/pages/SingleQuestionPage/SingleQuestionPage.jsx
@@ -2,19 +2,64 @@ import classes from "./SingleQuestionPage.module.css";
 import logo from "../../assets/tinder-logo-red.png";
 import { useState, useEffect } from "react";
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
-import { Link } from "react-router-dom";
+import { Link, useHref } from "react-router-dom";
 
-const answersArray = ["Man", "Woman", "Answer3", "Answer4"];
+
+
 const SingleQuestionPage = () => {
-  const [isClicked0, setClicked0] = useState(false);
-  const [isClicked1, setClicked1] = useState(false);
-  const [isClicked2, setClicked2] = useState(false);
-  const [isClicked3, setClicked3] = useState(false);
+
 
   const [isClicked, setClicked] = useState([false, false, false, false]);
-
+  const [questionIndex, setQuestionIndex] = useState(0);
   const [counter, setCounter] = useState(0);
   const [isContinueDisabled, setIsContinueDisabled] = useState(true);
+
+  const questionData=[
+    {
+      question:"Jestem:",
+      answers:["Mężczyzną","Kobieta","Helikopterem bojowym"]
+    },
+    {
+      question:"Pytanie dwa:",
+      answers:["odp1","odp2","odp3","odp4"]
+    },
+    {
+      question:"Pytanie trzy:",
+      answers:["odp1","odp2"]
+    }
+      
+  ];
+
+//Redux (store) może pomoże przekazywaniem zmiennych 
+  const ShowQuestion=(questionData, questionIndex)=>{
+    return (
+      <>
+        <header>
+                <h1 className={classes.Question}>{questionData[questionIndex].question}</h1>
+        </header>
+        <section className={classes.Answers}>
+        <ul>
+          {questionData[questionIndex].answers.map((item, index)=>(
+              <li
+              className={isClicked[index] ? classes.Active : classes.Deactive}
+              onClick={() => changeAnswerHandler(index)}
+            >
+              {item}
+            </li>
+          ))}
+        </ul>
+        </section>
+      </>
+    )
+  }
+  
+  const newQuestion=()=>{
+    if(questionIndex<3){
+    setQuestionIndex(questionIndex+1);
+    }
+    setClicked([false,false,false,false])
+    setCounter(0);
+  }
 
   useEffect(() => {
     console.log(counter);
@@ -51,32 +96,11 @@ const SingleQuestionPage = () => {
               <ChevronLeftIcon style={{ fontSize: "60px", color: "gray" }} />
             </button>
           </Link>
-          <h1 className={classes.Question}>I am a</h1>
         </header>
-        <section className={classes.Answers}>
-          <ul>
-            <li
-              className={isClicked[0] ? classes.Active : classes.Deactive}
-              onClick={() => changeAnswerHandler(0)}
-            >
-              {answersArray[0]}
-            </li>
-            <li
-              className={isClicked[1] ? classes.Active : classes.Deactive}
-              onClick={() => changeAnswerHandler(1)}
-            >
-              {answersArray[1]}
-            </li>
-            <li
-              className={isClicked[2] ? classes.Active : classes.Deactive}
-              onClick={() => changeAnswerHandler(2)}
-            >
-              {answersArray[2]}
-            </li>
-          </ul>
-        </section>
-        <Link to="/profiles" className={classes.Link}>
-          <button
+          {ShowQuestion(questionData,questionIndex)}
+        {/*wymyślić jak przejść do następnej strony, i zablokować wyjście poza tablice*/}
+         {/* <Link to="/profiles" className={classes.Link}> */}
+          <button onClick={newQuestion}
             className={
               counter === 1
                 ? classes.SubmitAnswer
@@ -86,7 +110,7 @@ const SingleQuestionPage = () => {
           >
             Kontynuuj
           </button>
-        </Link>
+        {/* </Link>  */}
       </main>
     </>
   );
