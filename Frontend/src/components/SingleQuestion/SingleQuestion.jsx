7bd209c37d6b4c596fd73fea11ae3f69edef3756
Sosek1 <68021413+Sosek1@users.noreply.github.com>
2023-06-05 18:30:31 +0200

    refactor: navigation between questions

--- Frontend/src/components/SingleQuestion/SingleQuestion.jsx
+++ Frontend/src/components/SingleQuestion/SingleQuestion.jsx
@@ -1,39 +1,45 @@
 import classes from "./SingleQuestion.module.css";
 import { useState } from "react";
+import { useQuestions } from "../../store/questions-context";
 
 const SingleQuestion = (props) => {
-  const [clickedAnswer, setClickedAnswer] = useState([
-    false,
-    false,
-    false,
-    false,
-  ]);
-  const [anyAnswerClicked, setAnyAnswerClicked] = useState(false);
-  const [markedAnswer, setMarkedAnswer] = useState("");
+  // const [clickedAnswer, setClickedAnswer] = useState([
+  //   false,
+  //   false,
+  //   false,
+  //   false,
+  // ]);
+  // const [anyAnswerClicked, setAnyAnswerClicked] = useState(false);
+  // const [markedAnswer, setMarkedAnswer] = useState("");
+
+  const { questionState, onUpdateQuestions } = useQuestions();
 
   const changeAnswerHandler = (index) => {
     let checkArray = [false, false, false, false];
-    for (let j = 0; j < clickedAnswer.length; j++) {
-      if (j === index) {
-        checkArray[j] = !clickedAnswer[j];
-        if (checkArray[j]) {
-          setAnyAnswerClicked(true);
-          setMarkedAnswer(
-            props.questionsData[props.questionsIndex].answers[index]
+    for (
+      let i = 0;
+      i < questionState[props.questionsIndex].clickedAnswer.length;
+      i++
+    ) {
+      if (i === index) {
+        checkArray[i] = !questionState[props.questionsIndex].clickedAnswer[i];
+        if (checkArray[i]) {
+          onUpdateQuestions(true, props.questionsIndex);
+          onUpdateQuestions(
+            props.questionsData[props.questionsIndex].answers[index],
+            props.questionsIndex
           );
         } else {
-          setAnyAnswerClicked(false);
+          onUpdateQuestions(false, props.questionsIndex);
         }
         break;
       }
     }
-    setClickedAnswer(checkArray);
+    onUpdateQuestions(checkArray, props.questionsIndex);
   };
 
   const changeQuestionHandler = () => {
-    props.nextQuestion(markedAnswer);
-    setClickedAnswer([false, false, false, false]);
-    setAnyAnswerClicked(false);
+    props.nextQuestion(questionState[props.questionsIndex].markedAnswer);
   };
 
   return (
@@ -48,7 +54,7 @@ const SingleQuestion = (props) => {
               <li
                 onClick={() => changeAnswerHandler(index)}
                 className={
-                  clickedAnswer[index]
+                  questionState[props.questionsIndex].clickedAnswer[index]
                     ? classes.activeAnswer
                     : classes.deactiveAnswer
                 }
@@ -63,11 +69,11 @@ const SingleQuestion = (props) => {
         <button
           onClick={changeQuestionHandler}
           className={
-            !anyAnswerClicked
+            !questionState[props.questionsIndex].anyAnswerClicked
               ? classes.submitAnswerDisabled
               : classes.submitAnswerEnabled
           }
-          disabled={!anyAnswerClicked}
+          disabled={!questionState[props.questionsIndex].anyAnswerClicked}
         >
           Kontynuuj
         </button>
--- Frontend/src/store/questions-context.jsx
+++ Frontend/src/store/questions-context.jsx
@@ -6,17 +6,89 @@ export const useQuestions = () => {
   return useContext(QuestionsContext);
 };
 
+const QUESTIONS_STATE = [
+  {
+    clickedAnswer: [false, false, false, false],
+    anyAnswerClicked: false,
+    markedAnswer: [],
+  },
+  {
+    clickedAnswer: [false, false, false, false],
+    anyAnswerClicked: false,
+    markedAnswer: [],
+  },
+  {
+    clickedAnswer: [false, false, false, false],
+    anyAnswerClicked: false,
+    markedAnswer: [],
+  },
+  {
+    clickedAnswer: [false, false, false, false],
+    anyAnswerClicked: false,
+    markedAnswer: [],
+  },
+  {
+    clickedAnswer: [false, false, false, false],
+    anyAnswerClicked: false,
+    markedAnswer: [],
+  },
+  {
+    clickedAnswer: [false, false, false, false],
+    anyAnswerClicked: false,
+    markedAnswer: [],
+  },
+  {
+    clickedAnswer: [false, false, false, false],
+    anyAnswerClicked: false,
+    markedAnswer: [],
+  },
+];
+
 export const QuestionContextProvider = (props) => {
   const [matchedGroup, setMatchedGroup] = useState([]);
+  const [questionState, setQuestionsState] = useState(QUESTIONS_STATE);
 
   const matchGroupHandler = (group) => {
     setMatchedGroup(group);
   };
-  console.log(matchedGroup);
+
+  const updateQuestionsStateHandler = (updatedProperty, questionIndex) => {
+    setQuestionsState((prevQuestionsState) => {
+      const updatedArr = [...prevQuestionsState];
+      let updatedObject = {};
+
+      if (Array.isArray(updatedProperty) && updatedProperty.length === 4) {
+        updatedObject = {
+          ...updatedArr[questionIndex],
+          clickedAnswer: updatedProperty,
+        };
+      }
+      if (Array.isArray(updatedProperty) && updatedProperty.length !== 4) {
+        updatedObject = {
+          ...updatedArr[questionIndex],
+          markedAnswer: updatedProperty,
+        };
+      }
+      if (typeof updatedProperty === "boolean") {
+        updatedObject = {
+          ...updatedArr[questionIndex],
+          anyAnswerClicked: updatedProperty,
+        };
+      }
+      updatedArr[questionIndex] = updatedObject;
+      console.log(updatedArr);
+      return updatedArr;
+    });
+  };
 
   return (
     <QuestionsContext.Provider
-      value={{ matchedGroup, onMatchGroup: matchGroupHandler }}
+      value={{
+        matchedGroup,
+        questionState,
+        onMatchGroup: matchGroupHandler,
+        onUpdateQuestions: updateQuestionsStateHandler,
+      }}
     >
       {props.children}
     </QuestionsContext.Provider>
