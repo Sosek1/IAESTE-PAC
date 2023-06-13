0ff0c7b21bbea0eec96f90ca00fd85490e31515a
Sosek1 <68021413+Sosek1@users.noreply.github.com>
2023-06-13 19:12:59 +0200

    create phone rotation detection

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,37 @@
+import { useEffect, useState } from "react";
 import { HashRouter as Router } from "react-router-dom";
 import AnimatedRoutes from "./components/AnimatedRoutes";
 import Warning from "./components/Warning/Warning";
 
 function App() {
+  const [showWarning, setShowWarning] = useState(null);
+  const [portraitWidth, setPortraitWidth] = useState(null);
+
+  useEffect(() => {
+    if (window.innerWidth < 500) {
+      setPortraitWidth(window.innerWidth);
+      setShowWarning(false);
+    }
+  }, []);
+
+  window.screen.orientation.addEventListener("change", (e) => {
+    let screenOrientation = e.currentTarget.type;
+
+    if (screenOrientation === "portrait-primary") {
+      setShowWarning(false);
+    } else if (screenOrientation === "landscape-primary") {
+      if (portraitWidth < 500) {
+        setShowWarning(true);
+      } else {
+        setShowWarning(false);
+      }
+    }
+  });
   return (
     <>
       <Router>
-        <AnimatedRoutes />
-        {/* <Warning /> */}
+        {!showWarning && <AnimatedRoutes />}
+        {showWarning && <Warning />}
       </Router>
     </>
   );
--- Frontend/src/components/Warning/Warning.jsx
+++ Frontend/src/components/Warning/Warning.jsx
@@ -3,11 +3,7 @@ import classes from "./Warning.module.css";
 const Warning = () => {
   return (
     <div className={classes.warningContainer}>
-      <h1>Uwaga!</h1>
-      <h2>
-        Aplikacja jest nie jest dostosowana do komputerów oraz tabletów, ze
-        względu na jej usecase
-      </h2>
+      <h1>Proszę obróć telefon</h1>
     </div>
   );
 };
